b46d6075a74429979c92e104ac70f144585d1104
weltenlicht <seb-klaus@web.de>
2021-04-24 21:16:54 +0200

    Lesson 67

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+
+    [SerializeField] AudioClip breakSound;
+
+    //Cached Reference
+    Level level;
+
     // Start is called before the first frame update
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        DestroyBlock();
     }
 
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        Destroy(gameObject);
+        level.BlockDestroyed();
+    }
 }
